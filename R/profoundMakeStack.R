4172ff9f513d90472a3a784029ef0f237bf35ac4
Aaron Robotham <aaron.robotham@uwa.edu.au>
2017-08-07 16:19:56 +0800

    Added stacking vignette and added mag-zero options to profoundMakeStack

--- R/profoundMakeStack.R
+++ R/profoundMakeStack.R
@@ -1,23 +1,34 @@
-profoundMakeStack=function(image_list, sky_list=NULL, skyRMS_list=NULL){
+profoundMakeStack=function(image_list, sky_list=NULL, skyRMS_list=NULL, magzero_in=0, magzero_out=0){
   if(is.list(image_list)){
-    if(!missing(sky_list)){
+    if(is.null(sky_list)==FALSE){
       if(length(image_list)!=length(sky_list)){stop('sky_list length does not match image_list length!')}
     }
-    if(!missing(skyRMS_list)){
+    if(is.null(skyRMS_list)==FALSE){
       if(length(image_list)!=length(skyRMS_list)){stop('skyRMS_list length does not match image_list length!')}
+    }
+    if(length(magzero_in)!=length(image_list)){
+      if(length(magzero_in)!=1){stop('Length of magzero_in should be the number of images in image_list or 1')}
+      magzero_in=rep(magzero_in,length(image_list))
     }
     stack=matrix(0,dim(image_list[[1]])[1],dim(image_list[[1]])[2])
     inv_var=0
     for(i in 1:length(image_list)){
       if(is.list(sky_list) & is.list(skyRMS_list)){
+        image_list[[i]]=image_list[[i]]*profoundMag2Flux(magzero_in[i],magzero_out)
+        sky_list[[i]]=sky_list[[i]]*profoundMag2Flux(magzero_in[i],magzero_out)
+        skyRMS_list[[i]]=skyRMS_list[[i]]*profoundMag2Flux(magzero_in[i],magzero_out)
         stack=stack+(image_list[[i]]-sky_list[[i]])/(skyRMS_list[[i]]^2)
         inv_var=inv_var+(1/skyRMS_list[[i]]^2)
       }
       if(is.list(sky_list) & is.list(skyRMS_list)==FALSE){
+        image_list[[i]]=image_list[[i]]*profoundMag2Flux(magzero_in[i],magzero_out)
+        sky_list[[i]]=sky_list[[i]]*profoundMag2Flux(magzero_in[i],magzero_out)
         stack=stack+(image_list[[i]]-sky_list[[i]])
         inv_var=1
       }
       if(is.list(sky_list)==FALSE & is.list(skyRMS_list)){
+        image_list[[i]]=image_list[[i]]*profoundMag2Flux(magzero_in[i],magzero_out)
+        skyRMS_list[[i]]=skyRMS_list[[i]]*profoundMag2Flux(magzero_in[i],magzero_out)
         stack=stack+image_list[[i]]/(skyRMS_list[[i]]^2)
         inv_var=inv_var+(1/skyRMS_list[[i]]^2)
       }
@@ -40,5 +51,5 @@ profoundMakeStack=function(image_list, sky_list=NULL, skyRMS_list=NULL){
       skyRMS=1
     }
   }
-return=list(image=stack, skyRMS=skyRMS)
+return=list(image=stack, skyRMS=skyRMS, magzero=magzero_out)
 }
