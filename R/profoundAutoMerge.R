2b51bb1193f4b7abd1910c61c76a2f2cbdb130e7
Aaron Robotham <aaron.robotham@uwa.edu.au>
2021-03-26 14:14:19 +0800

    Force to data.frame.

--- R/profoundAutoMerge.R
+++ R/profoundAutoMerge.R
@@ -1,4 +1,6 @@
-profoundAutoMerge = function(segim, segstats, spur_lim=4e-3, col_lim=NULL){
+profoundAutoMerge = function(segim, segstats, spur_lim=4e-3, col_lim=NULL, Ncut=1){
+  
+  segstats = as.data.frame(segstats)
   
   if(is.null(segstats$col) | is.null(col_lim)){
     sel = which(segstats$cenfrac / (1 + segstats$sep) < spur_lim
@@ -14,14 +16,18 @@ profoundAutoMerge = function(segim, segstats, spur_lim=4e-3, col_lim=NULL){
   
   if(length(sel) > 0){
     merge_segIDS = segstats[sel,"segID"]
-    segim_temp = segim
-    segim_temp[!segim_temp %in% merge_segIDS] = 0
-    group = profoundSegimGroup(segim_temp)$groupsegID
-    if(any(group$Ngroup==1)){
-      merge_segIDS = merge_segIDS[!merge_segIDS %in% group[group$Ngroup==1,"groupID"]]
+    group_old = profoundSegimGroup(segim)$groupsegID
+    merge_segIDS = merge_segIDS[!merge_segIDS %in% group_old[group_old$Ngroup == 1,'groupID']]
+    if(length(merge_segIDS) > 0){
+      segim_temp = segim
       segim_temp[!segim_temp %in% merge_segIDS] = 0
-      if(any(segim_temp > 0)){
-        group = profoundSegimGroup(segim_temp)$groupsegID
+      group = profoundSegimGroup(segim_temp)$groupsegID
+      if(any(group$Ngroup <= Ncut)){
+        merge_segIDS = merge_segIDS[!merge_segIDS %in% group[group$Ngroup <= Ncut,"groupID"]]
+        segim_temp[!segim_temp %in% merge_segIDS] = 0
+        if(any(segim_temp > 0)){
+          group = profoundSegimGroup(segim_temp)$groupsegID
+        }
       }
     }
   }
