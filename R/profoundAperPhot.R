978be0557143d8773f1d40afe745da63677acb15
Aaron Robotham <aaron.robotham@uwa.edu.au>
2025-10-07 13:45:08 +0800

    All NA segment will now return NA flux (I think that makes more sense)

--- R/profoundAperPhot.R
+++ R/profoundAperPhot.R
@@ -1,36 +1,62 @@
-.fluxcalcapp = function(x=NULL, y=NULL, flux=NULL, xcen=NA, ycen=NA, rad_app=NULL, centype='mean'){
-  if(is.na(xcen)){
-    if(centype == 'wt' | centype == 'mean'){
-      xcen = .meanwt(x, flux)
-    }else if(centype == 'max'){
-      xcen = x[which.max(flux)]
-    }else{
-      stop('centype must be max or mean!')
+.fluxcalcapp = function(x=NULL, y=NULL, rad2=NULL, flux=NULL, xcen=NA, ycen=NA, rad_app=NULL, centype='mean'){
+  if(is.null(rad2)){
+    if(is.na(xcen)){
+      if(centype == 'wt' | centype == 'mean'){
+        xcen = .meanwt(x, flux)
+      }else if(centype == 'max'){
+        xcen = x[which.max(flux)]
+      }else{
+        stop('centype must be max or mean!')
+      }
     }
-  }
-  
-  if(is.na(ycen)){
-    if(centype == 'wt' | centype == 'mean'){
-      ycen = .meanwt(y, flux)
-    }else if(centype == 'max'){
-      ycen = y[which.max(flux)]
-    }else{
-      stop('centype must be max or mean!')
+    
+    if(is.na(ycen)){
+      if(centype == 'wt' | centype == 'mean'){
+        ycen = .meanwt(y, flux)
+      }else if(centype == 'max'){
+        ycen = y[which.max(flux)]
+      }else{
+        stop('centype must be max or mean!')
+      }
     }
-  }
-  
-  if(xcen == 0 & ycen == 0){
-    rad2 = x^2 + y^2
+    
+    if(xcen == 0 & ycen == 0){
+      rad2 = x^2 + y^2
+    }else{
+      rad2 = (x - xcen)^2 + (y - ycen)^2 
+    }
   }else{
-    rad2 = (x - xcen)^2 + (y - ycen)^2 
+    xcen = 0
+    ycen = 0
   }
   
-  sel = which(rad2 < rad_app^2)
-  rad_out = max(rad2[sel])
-  sel_out = which(rad2 == rad_out)
-  
-  flux_app = sum(flux[sel], na.rm=TRUE)
-  flux_min = mean(flux[sel_out], na.rm=TRUE)
+  if(is.na(rad_app)){
+    Nsel = length(rad2)
+    rad_out = max(rad2)
+    sel_out = which(rad2 == rad_out)
+    
+    flux_app = sum(flux, na.rm=TRUE)
+    flux_min = mean(flux, na.rm=TRUE)
+  }else{
+    sel = which(rad2 <= rad_app^2)
+    Nsel = length(sel)
+    
+    if(length(Nsel) == 0){
+      flux_app = NA_real_
+      flux_min = 0
+    }else{
+      if(all(is.na(flux[sel]))){
+        flux_app = NA_real_
+        flux_min = 0
+      }else{
+        rad_out = max(rad2[sel])
+        sel_out = which(rad2 == rad_out)
+        
+        flux_app = sum(flux[sel], na.rm=TRUE)
+        flux_min = mean(flux[sel_out], na.rm=TRUE)
+      }
+    }
+  }
   
   if(!isTRUE(is.finite(flux_min))){ #this catches NA, NaN, NULL, Inf events
     flux_min = 0 #don't want to penalise when masked or other weird events
@@ -38,7 +64,7 @@
     flux_min = 0 #don't want to penalise when in the sky noise 
   }
   
-  return(list(flux_app=flux_app, flux_min=flux_min, N=length(sel)))
+  return(list(flux_app=flux_app, flux_min=flux_min, N=Nsel))
 }
 
 profoundAperPhot = function(image=NULL, segim=NULL, app_diam=1, keyvalues=NULL, tar=NULL,
@@ -96,19 +122,24 @@ profoundAperPhot = function(image=NULL, segim=NULL, app_diam=1, keyvalues=NULL, 
     stop('fluxtype must be Jansky / Microjansky / Raw!')
   }
   
-  segID = x = y = flux = j = NULL
+  segID = x = y = flux = j = rad2 = NULL
   
   Rapp = (app_diam / 2 / pixscale)
   Aapp = (pi * Rapp^2)
   
-  if(is.null(tar$segID)){
+  
+  if(is.null(tar)){
     segsel = which(segim > 0, arr.ind = TRUE)
   }else{
     tar = as.data.frame(tar)
     
-    segsel = segim %in% tar$segID
-    dim(segsel) = dim(segim)
-    segsel = which(segsel, arr.ind = TRUE)
+    if(is.null(tar$xcen) & !is.null(tar$xmax)){
+      stop('Fibre x position must be called xcen (not xmax)!')
+    }
+    
+    if(is.null(tar$ycen) & !is.null(tar$ymax)){
+      stop('Fibre y position must be called ycen (not ymax)!')
+    }
     
     if(!is.null(tar$xcen)){
       # + 0.5 since arr.ind is offset compared to our normal image definition
@@ -119,7 +150,43 @@ profoundAperPhot = function(image=NULL, segim=NULL, app_diam=1, keyvalues=NULL, 
       # + 0.5 since arr.ind is offset compared to our normal image definition
       tar[,'ycen'] = tar[,'ycen'] + 0.5
     }
+    
+    if(!is.null(tar$xcen) & !is.null(tar$ycen) & is.null(tar$segID)){
+      tar$segID = segim[as.matrix(tar[,c('xcen', 'ycen')])]
+    }
+    
+    if(any(tar$segID == 0L)){
+      stop('Some fibres are not on a segment!')
+    }
+    
+    if(anyDuplicated(tar$segID)){
+      stop('Not all fibre positions fall on a unique segment!')
+    }
+    
+    segsel = segim %in% tar$segID
+    dim(segsel) = dim(segim)
+    segsel = which(segsel, arr.ind = TRUE)
   }
+  
+  # if(is.null(tar$segID)){
+  #   segsel = which(segim > 0, arr.ind = TRUE)
+  # }else{
+  #   tar = as.data.frame(tar)
+  #   
+  #   segsel = segim %in% tar$segID
+  #   dim(segsel) = dim(segim)
+  #   segsel = which(segsel, arr.ind = TRUE)
+  #   
+  #   if(!is.null(tar$xcen)){
+  #     # + 0.5 since arr.ind is offset compared to our normal image definition
+  #     tar[,'xcen'] = tar[,'xcen'] + 0.5
+  #   }
+  #   
+  #   if(!is.null(tar$ycen)){
+  #     # + 0.5 since arr.ind is offset compared to our normal image definition
+  #     tar[,'ycen'] = tar[,'ycen'] + 0.5
+  #   }
+  # }
   
   segID_all = as.integer(segim[segsel])
   
@@ -149,21 +216,23 @@ profoundAperPhot = function(image=NULL, segim=NULL, app_diam=1, keyvalues=NULL, 
     if(verbose){message('User provided tar table of fibre positions')}
   }
   
-  # for(i in 1:dim(tar)[1]){
-  #   tempDT[segID == tar[i,'segID'], x := x - tar[i,'xcen']]
-  #   tempDT[segID == tar[i,'segID'], y := y - tar[i,'ycen']]
-  # }
+  if(verbose){message('Placing fibres and sorting')}
   
   match_segID = match(tempDT$segID, tar$segID)
   tempDT[, x:= x - tar[match_segID, 'xcen']]
   tempDT[, y:= y - tar[match_segID, 'ycen']]
+  tempDT[, rad2:= x^2 + y^2]
+  
+  setkey(tempDT, segID, rad2)
   
   if(verbose){message('Computing fibre photometry...')}
   
   output = foreach(j = seq_along(Rapp), .combine='cbind')%do%{
     #newer more accurate fibre mag calculation
     if(verbose){message('  Aperture: ', app_diam[j], ' [asec]')}
-    temp_app = tempDT[, .fluxcalcapp(x=x, y=y, flux=flux, xcen=0, ycen=0, rad_app=Rapp[j]), by=segID]
+    # the top one can completely remove segments in some weird edge cases, and doesn't appear to be faster. Use the second!
+    #temp_app = tempDT[rad2 <= Rapp[j]^2, .fluxcalcapp(x=x, y=y, rad2=rad2, flux=flux, xcen=0, ycen=0, rad_app=NA), by=segID]
+    temp_app = tempDT[, .fluxcalcapp(x=x, y=y, rad2=rad2, flux=flux, xcen=0, ycen=0, rad_app=Rapp[j]), by=segID]
     
     if(correction){
       temp_app$flux_app = temp_app$flux_app - (temp_app$N - Aapp[j])*temp_app$flux_min
