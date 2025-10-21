e671c2e39ab0a8a4a8cddc443bd1c04cc4914953
Aaron Robotham <aaron.robotham@uwa.edu.au>
2025-10-21 12:20:55 +0800

    Change to poly_x/y

--- src/poly_cover.cpp
+++ src/poly_cover.cpp
@@ -7,14 +7,14 @@
 using namespace Rcpp;
 
 // [[Rcpp::export(".point_in_polygon_cpp_short")]]
-double in_poly(double testx, double testy, NumericVector vertx, NumericVector verty)
+double in_poly(double testx, double testy, NumericVector poly_x, NumericVector poly_y)
 {
   int i, j;
   double temp = 0.0;
   
-  for (i = 0, j = vertx.size() - 1; i < vertx.size(); j = i++) {
-    if ( ((verty[i] > testy) != (verty[j] > testy)) &&
-         (testx < (vertx[j] - vertx[i]) * (testy - verty[i]) / (verty[j] - verty[i]) + vertx[i]) )
+  for (i = 0, j = poly_x.size() - 1; i < poly_x.size(); j = i++) {
+    if ( ((poly_y[i] > testy) != (poly_y[j] > testy)) &&
+         (testx < (poly_x[j] - poly_x[i]) * (testy - poly_y[i]) / (poly_y[j] - poly_y[i]) + poly_x[i]) )
       temp = !temp;
   }
   
@@ -22,9 +22,9 @@ double in_poly(double testx, double testy, NumericVector vertx, NumericVector ve
 }
 
 // Recursive function to determine fractional pixel coverage
-double pixelCoverPoly(double x, double y, NumericVector vertx, NumericVector verty, int depth) {
+double pixelCoverPoly(double x, double y, NumericVector poly_x, NumericVector poly_y, int depth) {
   if (depth == 0) {
-    return in_poly(x, y, vertx, verty);
+    return in_poly(x, y, poly_x, poly_y);
   }
   
   double quarter = 0.25 / (1 << (depth - 1)); // (1 << (depth - 1)) is equivalent to pow(2, depth - 1), but faster
@@ -35,10 +35,10 @@ double pixelCoverPoly(double x, double y, NumericVector vertx, NumericVector ver
   double y_min  = y - quarter;
   double y_plus = y + quarter;
   
-  coverage += pixelCoverPoly(x_min, y_min, vertx, verty, depth - 1);
-  coverage += pixelCoverPoly(x_min, y_plus, vertx, verty, depth - 1);
-  coverage += pixelCoverPoly(x_plus, y_min, vertx, verty, depth - 1);
-  coverage += pixelCoverPoly(x_plus, y_plus, vertx, verty, depth - 1);
+  coverage += pixelCoverPoly(x_min, y_min, poly_x, poly_y, depth - 1);
+  coverage += pixelCoverPoly(x_min, y_plus, poly_x, poly_y, depth - 1);
+  coverage += pixelCoverPoly(x_plus, y_min, poly_x, poly_y, depth - 1);
+  coverage += pixelCoverPoly(x_plus, y_plus, poly_x, poly_y, depth - 1);
   
   return coverage / 4.0;
 }
@@ -46,18 +46,18 @@ double pixelCoverPoly(double x, double y, NumericVector vertx, NumericVector ver
 // [[Rcpp::export]]
 NumericVector profoundPolyCover(NumericVector x,
                                 NumericVector y,
-                                NumericVector vertx,
-                                NumericVector verty,
+                                NumericVector poly_x,
+                                NumericVector poly_y,
                                 int depth = 4,
                                 int nthreads = 1) {
   
   const int n = x.size();
   NumericVector result(n);
   
-  double vertx_min = min(vertx) - 0.5;
-  double vertx_max = max(vertx) + 0.5;
-  double verty_min = min(verty) - 0.5;
-  double verty_max = max(verty) + 0.5;
+  double poly_x_min = min(poly_x) - 0.5;
+  double poly_x_max = max(poly_x) + 0.5;
+  double poly_y_min = min(poly_y) - 0.5;
+  double poly_y_max = max(poly_y) + 0.5;
   
   #ifdef _OPENMP
     // Parallelize the main loop. Use 'if' to avoid overhead for tiny n.
@@ -65,9 +65,9 @@ NumericVector profoundPolyCover(NumericVector x,
   #endif
   for (int i = 0; i < n; ++i) {
     // Make loop-local copies to avoid data races
-    if (x[i] > vertx_min && x[i] < vertx_max) {
-      if (y[i] > verty_min && y[i] < verty_max) {
-        result[i] = pixelCoverPoly(x[i], y[i], vertx, verty, depth);
+    if (x[i] > poly_x_min && x[i] < poly_x_max) {
+      if (y[i] > poly_y_min && y[i] < poly_y_max) {
+        result[i] = pixelCoverPoly(x[i], y[i], poly_x, poly_y, depth);
       } else {
         result[i] = 0.0;
       }
